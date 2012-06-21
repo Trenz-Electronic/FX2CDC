8bf84ce25342d24a93ba45f1318aaee0ddf7345a
Oleksandr Kienko <a.kienko@gmail.com>
2012-06-21 11:52:58 +0300

    Minor changes

--- Virtual.c
+++ Virtual.c
@@ -19,6 +19,10 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
 IN THE SOFTWARE.
 */
+/*
+-- Company: Trenz Electronics GmbH
+-- Engineer: Oleksandr Kiyenko (a.kienko@gmail.com)
+*/
 
 #pragma NOIV               // Do not generate interrupt vectors
 #include "fx2.h"
